88fe55152cde446df7425469f8a3328a7b5662c9
Sebastian Zarate <zaratesanmiguel2019@outlook.com>
2024-03-18 02:46:50 -0500

    Removed Un. Import

--- dmpapp/lib/widgets/product_widgets/product_widget_list.dart
+++ dmpapp/lib/widgets/product_widgets/product_widget_list.dart
@@ -1,6 +1,5 @@
 import 'package:dmpapp/models/product.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class ProductWidgetList extends StatelessWidget {
   final Product product;
